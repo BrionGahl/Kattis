4cd8934bb0e8c2a5220747c83a0c74325d4213bc
Brion Gahl <BrionGahl@gmail.com>
2022-05-03 03:49:29 -0500

    time loop

---
+++ python/easy/time_loop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+def abracadabra(n):
+    for i in range(n):
+        print(f"{i+1} Abracadabra")
+    return
+def main():
+    abracadabra(int(input()))
+    return
+
+if __name__ == "__main__":
+    main()
